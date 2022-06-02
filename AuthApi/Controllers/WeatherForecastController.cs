169c6290b620ea37bc02d95300c9070bb2caea0a
Tho Nguyen <33661389+truongtho-dev@users.noreply.github.com>
2022-06-02 22:59:34 +0700

    Update WeatherForecastController.cs

--- AuthApi/Controllers/WeatherForecastController.cs
+++ AuthApi/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace AuthApi.Controllers
 	{
 		private static readonly string[] Summaries = new[]
 		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "HCM"
 		};
 
 		private readonly ILogger<WeatherForecastController> _logger;
